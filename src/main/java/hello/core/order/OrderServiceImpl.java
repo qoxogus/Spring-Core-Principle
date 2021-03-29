167deff0e9bdcc9d232846ea6aa51cb8481aa9ef
qoxogus <a01034367055@gmail.com>
2021-03-29 18:22:44 +0900

    [주문과 할인 도메인 개발]

---
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,21 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository(); //메모리회원리포
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy(); //고정할인 금액정책
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) { //주문생성 요청이 오면
+        Member member = memberRepository.findById(memberId); //회원정보를 먼저 조회를 하고
+        int discountPrice = discountPolicy.discount(member, itemPrice);// 할인정책에 회원을 넘긴다
+
+        return new Order(memberId, itemName, itemPrice, discountPrice); //최종 생성된 주문 반환
+    }
+}
