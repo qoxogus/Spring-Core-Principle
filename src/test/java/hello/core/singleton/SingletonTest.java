e525b1397abbe9ee1487415586bff8f9ff516476
qoxogus <a01034367055@gmail.com>
2021-04-06 10:11:11 +0900

    [singleton test err fix]

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -52,11 +52,11 @@ public class SingletonTest {
         MemberService memberService1 = ac.getBean("memberService", MemberService.class);
         MemberService memberService2 = ac.getBean("memberService", MemberService.class);
 
-        //참조값이 다른것을 확인
+        //참조값이 같은것을 확인
         System.out.println("memberService1 = " + memberService1);
         System.out.println("memberService2 = " + memberService2);
 
-        //memberService1 != memberService2
+        //memberService1 == memberService2
         assertThat(memberService1).isSameAs(memberService2);
     }
 }
